93382488e02920a9e7c86dac5e176084accbff6e
NappyNexus <gabrielsoriano172@gmail.com>
2024-11-06 22:18:33 -0400

    scrollable cards done

---
+++ app.js
@@ -0,0 +1,15 @@
+// const faqs = document.querySelectorAll(".arrow_title");
+
+// faqs.forEach(arrow_title => {
+//     arrow_title.addEventListener("click", () => {
+//         arrow_title.classList.toggle("active");
+//     })
+// })
+
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach(faq => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("active");
+  })
+})
