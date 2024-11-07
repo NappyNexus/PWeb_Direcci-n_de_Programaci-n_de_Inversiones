da539a83f2b94e5f19cbd50139c78f7adc86a496
NappyNexus <gabrielsoriano172@gmail.com>
2024-11-07 00:21:17 -0400

    footer and main page done

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-// const faqs = document.querySelectorAll(".arrow_title");
-
-// faqs.forEach(arrow_title => {
-//     arrow_title.addEventListener("click", () => {
-//         arrow_title.classList.toggle("active");
-//     })
-// })
-
 const faqs = document.querySelectorAll(".faq");
 
 faqs.forEach(faq => {
